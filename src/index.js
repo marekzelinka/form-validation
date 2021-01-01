ce62b40c90ff36f0d41e44c90e7a100ffd5ace66
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-01-01 11:11:23 +0100

    cleanup code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const form = document.querySelector("#signup-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
 document.querySelectorAll(".textfield").forEach((field) => {
   field.addEventListener("change", (event) => {
     event.currentTarget.classList.add("dirty");
   });
-});
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
 });
